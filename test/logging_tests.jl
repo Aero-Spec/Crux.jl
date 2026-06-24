64f86ff7a42cb7a8779f02f81bedea3c8fdcb68e
Anshrin Srivastava <Anshrin@gmail.com>
2026-06-24 04:39:12 -0400

    Update logging_tests.jl

--- test/logging_tests.jl
+++ test/logging_tests.jl
@@ -1,10 +1,33 @@
-using Crux
-using Test
+@testset "logging callback coverage" begin
+    buffer = Dict(
+        :reward => [1.0, 2.0, 3.0, 4.0],
+        :episode_end => [1, 1, 1, 1]
+    )
 
-## elapsed
-@test Crux.elapsed(1000, 100) 
-@test !Crux.elapsed(1001, 100) 
-@test Crux.elapsed(47001:47501, 500)
-@test !Crux.elapsed(47001:47499, 500)
+    s = (; buffer = buffer)
 
+    avg_cb = log_episode_averages([:reward], 2)
+    avg_result = avg_cb(s)
 
+    @test haskey(avg_result, Symbol("avg_reward"))
+    @test avg_result[Symbol("avg_reward")] ≈ 3.5
+
+    sum_cb = log_experience_sums([:reward], 2)
+    sum_result = sum_cb(s)
+
+    @test haskey(sum_result, Symbol("avg_reward"))
+    @test sum_result[Symbol("avg_reward")] ≈ 7.0
+end
+
+@testset "save_gif early return coverage" begin
+    cb = save_gif(log_at_zero = false)
+
+    s = (
+        dir = tempdir(),
+        mdp = nothing,
+        agent = (; π = nothing),
+        logger = nothing
+    )
+
+    @test cb(0, s) === nothing
+end
