e5b2268be129611e776e5919d170a8faa6036629
Anshrin Srivastava <Anshrin@gmail.com>
2026-06-25 04:08:26 -0400

    Update runtests.jl

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,44 +1,54 @@
 using Test, CUDA, Crux
 
-## Only run CUDA tests if it seems to be working properly
-USE_CUDA = false
-try
-	cu(zeros(Float32, 10, 10))
-	USE_CUDA = true
-	CUDA.allowscalar(false)
-catch end
+global USE_CUDA = false
 
-## Run functionality tests
+try
+    cu(zeros(Float32, 10, 10))
+    global USE_CUDA = true
+    CUDA.allowscalar(false)
+catch
+end
+
 @testset "README example" begin
-	include("readme.jl")
+    include("readme.jl")
 end
+
 @testset "spaces" begin
-	include("spaces_tests.jl")
+    include("spaces_tests.jl")
 end
-USE_CUDA && @testset "devices" begin
-	include("devices_tests.jl")
+
+@testset "devices" begin
+    include("devices_tests.jl")
 end
+
 @testset "util" begin
-	include("util_tests.jl")
+    include("util_tests.jl")
 end
+
 @testset "buffer" begin
-	include("experience_buffer_tests.jl")
+    include("experience_buffer_tests.jl")
 end
+
 @testset "policy" begin
-	include("policy_tests.jl")
+    include("policy_tests.jl")
 end
+
 @testset "logging" begin
-	include("logging_tests.jl")
+    include("logging_tests.jl")
 end
+
 @testset "extras" begin
-	include("extras_tests.jl")
+    include("extras_tests.jl")
 end
+
 @testset "solver" begin
-	include("gym/solver_tests.jl")
+    include("gym/solver_tests.jl")
 end
+
 @testset "sampler" begin
-	include("gym/sampler_tests.jl")
+    include("gym/sampler_tests.jl")
 end
+
 @testset "analysis" begin
-	include("analysis.jl")
+    include("analysis.jl")
 end
